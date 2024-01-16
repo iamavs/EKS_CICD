501ed7af640b9ff759ae2454a0a65de71f789e8f
root <root@ip-172-31-22-213.ec2.internal>
2024-01-16 18:42:28 +0000

    Updated thecode

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!-CodeBuilkd')
+  res.send('Hello World!-CodeBuilkd----')
 })
 
 app.get('/status', (req, res) => {
