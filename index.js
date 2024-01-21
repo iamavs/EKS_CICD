d0e249d02a1ba3778972c4341c8e96c6c6a61e31
root <root@ip-172-31-22-213.ec2.internal>
2024-01-21 14:46:13 +0000

    c

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!-CodeBuilkd----added policy')
+  res.send('Hello World!-CodeBuilkd----added policyy')
 })
 
 app.get('/status', (req, res) => {
